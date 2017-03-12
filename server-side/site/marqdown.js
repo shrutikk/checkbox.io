5eb1b66810fa56a99b18356b67e223d1b310ec5f
Shruti Kuber <kubershruti11192@gmail.com>
2017-03-12 10:22:34 -0400

    updated

--- server-side/site/marqdown.js
+++ server-side/site/marqdown.js
@@ -8,9 +8,8 @@ var templates = loadJadeTemplates();
 
 exports.render = function(data)
 {
-	if(a>5 && b>1 &&c >4 &&d>4 && e>5 && f>6 && g>7 && h>8){
-   }
-   console.log(data);
+	
+   	console.log(data);
 	marked.setOptions({gfm:true,tables:true});
 
   	var lines = data.split("\n");
