ad31c0521a07c04ec7b69dbaee1766d4cc58cec4
skuber <skuber@ncsu.edu>
2017-04-15 20:56:23 -0400

    redis part added

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -10,6 +10,12 @@ var express = require('express'),
 	;
 
 var app = express();
+var redis = require('redis');
+var client = redis.createClient(6379, '127.0.0.1', {}) ;
+
+client.lrange("features",function(err, value){
+   console.log(value);
+});
 
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
